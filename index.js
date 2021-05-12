c8215d438627719b81fb59be9197fb765aa96867
Phillo93 <philipp.frentzen@gmail.com>
2021-05-12 13:07:03 +0200

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config()
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+// parse application/json
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send("main object");
+})
+app.listen(PORT, () => {
+    console.log('listening at http:localhost:5000');
+})
