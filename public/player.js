d41a8a838443402a2925716cf3cf80c3a510c090
Steffen Klömpges <steffenklompges@MacBook-Air-von-Steffen.local>
2021-05-18 15:54:14 +0200

    addStyles/FunctionPlayer

--- public/player.js
+++ public/player.js
@@ -1,7 +1,26 @@
-document.querySelector('#playaudio').onclick = function () {
-    audio.play();
+var posSlider = document.getElementById("timeline");
+var audio = document.getElementById("player");
+let playButton = document.getElementById('play_button')
+let pauseButton = document.getElementById('pause_button')
+
+audio.addEventListener("loadedmetadata", setSliderMax);
+
+function setSliderMax() {
+    posSlider.setAttribute('max', audio.duration);
 }
 
-document.querySelector('#pauseaudio').onclick = function () {
-    audio.pause();
+audio.addEventListener("timeupdate", setSliderVal);
+
+function setSliderVal() {
+    posSlider.value = audio.currentTime;
 }
+
+playButton.addEventListener('click', () => {
+    playButton.style.display = 'none';
+    pauseButton.style.display = 'inline-block';
+})
+
+pauseButton.addEventListener('click', () => {
+    pauseButton.style.display = 'none';
+    playButton.style.display = 'inline-block';
+})
